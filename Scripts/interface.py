37da5a77df3f7eedc3398a28539249e2faff6f40
“Robertzzel” <robertzel_2001@yahoo.com>
2022-01-19 01:28:35 +0200

    Created functions for creating Widgets

--- Scripts/interface.py
+++ Scripts/interface.py
@@ -19,157 +19,79 @@ class Interface:
         self.window = Tk()
         self.window.geometry("830x720")
 
-        self.logging_data_state = 'disabled'
-        self.host_name = StringVar()
-        self.address_request = StringVar()
-        self.client_id = StringVar()
-        self.hardware_address = StringVar()
-        self.client_ip_address = StringVar()
-        self.subnet_mask = BooleanVar()
-        self.router = BooleanVar()
-        self.domain_server = BooleanVar()
-        self.broadcast_address = BooleanVar()
-        self.lease_time = BooleanVar()
-        self.renewal_time = BooleanVar()
-        self.subnet_mask_option = StringVar()
-        self.router_option = StringVar()
-        self.domain_server_option = StringVar()
-        self.broadcast_address_option = StringVar()
-        self.lease_time_option = StringVar()
-        self.renewal_time_option = StringVar()
-        self.logging_data = StringVar()
-        self.lease_time_value = IntVar()
-        self.renewal_time_value = IntVar()
-        self.clock_value = StringVar()
-        self.ip_curent_value = StringVar()
+        self.buton_connect = self.create_button(text="CONNECT",
+                                                command=lambda: threading.Thread(target=self.connect, args=()).start(), x_position=20, y_position=20)
+        self.buton_generare_default = self.create_button(text="GEN. DEFAULT",
+                                                         command=lambda: threading.Thread(target=self.generate_default, args=()).start(), x_position=100, y_position=20)
+        self.buton_deconectare = self.create_button(text="DISCONNECT",
+                                                    command=lambda: threading.Thread(target=self.disconnect, args=()).start(), x_position=203, y_position=20)
 
-        self.subnet_mask_option.set('...')
-        self.router_option.set('...')
-        self.domain_server_option.set('...')
-        self.broadcast_address_option.set('...')
-        self.lease_time_option.set('...')
-        self.renewal_time_option.set('...')
-        self.ip_curent_value.set("None")
-        self.lease_time_value.set(0)
-        self.renewal_time_value.set(0)
+        self.check_box_subnet_mask, self.subnet_mask = self.create_checkbutton(text='Subnet Mask', x_pos=20, y_pos=280)
+        self.check_box_router, self.router = self.create_checkbutton(text="Router", x_pos=20, y_pos=320)
+        self.check_box_domain_server, self.domain_server = self.create_checkbutton("Domain Server", 20, 360)
+        self.check_box_broadcast_address, self.broadcast_address = self.create_checkbutton("Broadcast Address", 20, 400)
+        self.check_box_lease_time, self.lease_time = self.create_checkbutton("Lease Time", 20, 440)
+        self.check_box_renewal_time, self.renewal_time = self.create_checkbutton("Renewal Time", 20, 480)
 
-        self.buton_connect = Button(self.window, text="CONNECT",
-                                    command=lambda: threading.Thread(target=self.connect, args=()).start())
-        self.buton_generare_default = Button(self.window, text="GEN. DEFAULT",
-                                        command=lambda: threading.Thread(target=self.generate_default, args=()).start())
-        self.buton_deconectare = Button(self.window, text="DISCONNECT",
-                                        command=lambda: threading.Thread(target=self.disconnect, args=()).start())
+        self.label_host_name = self.create_label(x_pos=20, y_pos=70, text="HOST NAME")
+        self.label_address_request = self.create_label(20,110,text="ADDRESS REQUEST")
+        self.label_client_id = self.create_label(20,150,text="CLIENT ID")
+        self.label_client_hardware_address = self.create_label(20,190,text="MAC")
+        self.label_client_ip_address = self.create_label(20,230,text="CLIENT IP ADDRESS")
+        self.label_subnet_mask, self.subnet_mask_option = self.create_label(150,280,variable_type=StringVar)
+        self.label_router, self.router_option = self.create_label(150,320,variable_type=StringVar)
+        self.label_domain_server_option, self.domain_server_option = self.create_label(150,360,variable_type=StringVar)
+        self.label_broadcast_address_option, self.broadcast_address_option = self.create_label(150,400,variable_type=StringVar)
+        self.label_lease_time_option, self.lease_time_option = self.create_label(150,440,variable_type=StringVar)
+        self.label_renewal_time_option, self.renewal_time_option = self.create_label(150,480,variable_type=StringVar)
+        self.label_logging = self.create_label(400,46,text="Logging")
+        self.label_separator_footer = self.create_label(20,570,text="_"*122)
+        self.label_lease_time_value, self.lease_time_value = self.create_label(90,600,variable_type=IntVar)
+        self.label_lease_time = self.create_label(20,600,text="Lease Time:")
+        self.label_renewal_time_value, self.renewal_time_value = self.create_label(106,631,variable_type=IntVar)
+        self.label_renewal_time = self.create_label(20,630,text="Renewal time:")
+        self.label_clock, self.clock_value = self.create_label(197,661,variable_type=StringVar)
+        self.label_renew_date = self.create_label(20,660,text="Renew date")
+        self.label_ip_curent = self.create_label(400,690,text="IP curent")
+        self.label_ip_curent_value, self.ip_curent_value = self.create_label(453,690, variable_type=StringVar)
+        self.label_istoric_ips = self.create_label(400, 600, text="Istoric IP-uri")
 
-        self.check_box_subnet_mask = Checkbutton(self.window, text='Subnet Mask', variable=self.subnet_mask, onvalue=1, offvalue=0)
-        self.check_box_router = Checkbutton(self.window, text='Router', variable=self.router, onvalue=1, offvalue=0)
-        self.check_box_domain_server = Checkbutton(self.window, text='Domain Server', variable=self.domain_server, onvalue=1,
-                                              offvalue=0)
-        self.check_box_broadcast_address = Checkbutton(self.window, text='Broadcast address', variable=self.broadcast_address,
-                                                  onvalue=1, offvalue=0)
-        self.check_box_lease_time = Checkbutton(self.window, text='Lease Time', variable=self.lease_time, onvalue=1, offvalue=0)
-        self.check_box_renewal_time = Checkbutton(self.window, text='Renewal Time', variable=self.renewal_time, onvalue=1, offvalue=0)
+        self.input_host_name, self.host_name = self.create_entry(x_position=150, y_position=70, width=180, height=20)
+        self.input_address_request, self.address_request = self.create_entry(150,110,180,20)
+        self.input_client_id, self.client_id = self.create_entry(150,150,180,20)
+        self.input_client_hardware_address, self.hardware_address = self.create_entry(150,190,180,20)
+        self.input_client_ip_address, self.client_ip_address = self.create_entry(150,230,180,20)
 
-        self.label_host_name = Label(self.window, text="HOST NAME", font=("Arial", 8))
-        self.label_address_request = Label(self.window, text="ADDRESS REQUEST", font=("Arial", 8))
-        self.label_client_id = Label(self.window, text="CLIENT ID", font=("Arial", 8))
-        self.label_client_hardware_address = Label(self.window, text="MAC", font=("Arial", 8))
-        self.label_client_ip_address = Label(self.window, text="CLIENT IP ADDRESS", font=("Arial", 8))
-        self.label_subnet_mask = Label(self.window, textvariable=self.subnet_mask_option)
-        self.label_router = Label(self.window, textvariable=self.router_option)
-        self.label_domain_server_option = Label(self.window, textvariable=self.domain_server_option)
-        self.label_broadcast_address_option = Label(self.window, textvariable=self.broadcast_address_option)
-        self.label_lease_time_option = Label(self.window, textvariable=self.lease_time_option)
-        self.label_renewal_time_option = Label(self.window, textvariable=self.renewal_time_option)
-        self.label_logging = Label(self.window, text="Logging", font=("Arial", 10))
-        self.label_separator_footer = Label(self.window, text="_" * 122, font=("Arial", 8))
-        self.label_lease_time_value = Label(self.window, textvariable=self.lease_time_value)
-        self.label_lease_time = Label(self.window, text="Lease time:")
-        self.label_renewal_time_value = Label(self.window, textvariable=self.renewal_time_value)
-        self.label_renewal_time = Label(self.window, text="Renewal time:")
-        self.label_clock = Label(self.window, textvariable=self.clock_value)
-        self.label_renew_date = Label(self.window, text="Renew date: ")
-        self.label_istoric_ips = Label(self.window, text="Istoric ip-uri:")
-        self.label_ip_curent = Label(text="Ip curent:")
-        self.label_ip_curent_value = Label(self.window, textvariable=self.ip_curent_value)
-
-        self.input_host_name = Entry(self.window, textvariable=self.host_name, font=('calibre', 10, 'normal'))
-        self.input_address_request = Entry(self.window, textvariable=self.address_request, font=('calibre', 10, 'normal'))
-        self.input_client_id = Entry(self.window, textvariable=self.client_id, font=('calibre', 10, 'normal'))
-        self.input_client_hardware_address = Entry(self.window, textvariable=self.hardware_address, font=('calibre', 10, 'normal'))
-        self.input_client_ip_address = Entry(self.window, textvariable=self.client_ip_address, font=('calibre', 10, 'normal'))
-
-        self.text_logging = Text(self.window, height=30, width=49, state=self.logging_data_state)
-        self.text_istoric_ips = Text(self.window, height=3, width=49)
-
-    def set_widgets(self):
-        self.buton_connect.place(x=20, y=20)
-        self.buton_generare_default.place(x=100, y=20)
-        self.buton_deconectare.place(x=203, y=20)
-
-        self.check_box_subnet_mask.place(x=20, y=280)
-        self.check_box_router.place(x=20, y=320)
-        self.check_box_domain_server.place(x=20, y=360)
-        self.check_box_broadcast_address.place(x=20, y=400)
-        self.check_box_lease_time.place(x=20, y=440)
-        self.check_box_renewal_time.place(x=20, y=480)
-
-        self.label_host_name.place(x=20, y=70)
-        self.label_address_request.place(x=20, y=110)
-        self.label_client_id.place(x=20, y=150)
-        self.label_client_hardware_address.place(x=20, y=190)
-        self.label_client_ip_address.place(x=20, y=230)
-        self.label_subnet_mask.place(x=150, y=280)
-        self.label_router.place(x=150, y=320)
-        self.label_domain_server_option.place(x=150, y=360)
-        self.label_broadcast_address_option.place(x=150, y=400)
-        self.label_lease_time_option.place(x=150, y=440)
-        self.label_renewal_time_option.place(x=150, y=480)
-        self.label_logging.place(x=400, y=46)
-        self.label_separator_footer.place(x=20, y=570)
-        self.label_lease_time_value.place(x=90, y=600)
-        self.label_lease_time.place(x=20, y=600)
-        self.label_renewal_time_value.place(x=106, y=631)
-        self.label_renewal_time.place(x=20, y=630)
-        self.label_clock.place(x=197, y=661)
-        self.label_renew_date.place(x=20, y=660)
-        self.label_ip_curent.place(x=400, y=690)
-        self.label_ip_curent_value.place(x=453, y=690)
-        self.label_istoric_ips.place(x=400, y=600)
-
-        self.input_host_name.place(x=150, y=70, width=180, height=20)
-        self.input_address_request.place(x=150, y=110, width=180, height=20)
-        self.input_client_id.place(x=150, y=150, width=180, height=20)
-        self.input_client_hardware_address.place(x=150, y=190, width=180, height=20)
-        self.input_client_ip_address.place(x=150, y=230, width=180, height=20)
-
-        self.text_logging.place(x=400, y=70)
-        self.text_istoric_ips.place(x=400, y=630)
+        self.text_logging, self.logging_data_state = self.create_text(x_pos=400,y_pos=70,height=30,width=49,with_state=True)
+        self.text_istoric_ips = self.create_text(400,630,3,49)
 
     def create_button(self, text: str, command: Callable, x_position: int, y_position: int) -> Button:
         button = Button(self.window, text=text, command=command)
         button.place(x=x_position, y=y_position)
         return button
 
-    def create_entry(self, variable_type: Callable, x_position: int, y_position: int, width: int,
-                     height: int, font: tuple = ('calibre', 10, 'normal')) -> (Entry, Variable):
+    def create_entry(self, x_position: int, y_position: int, width: int,
+                     height: int, variable_type: Callable = StringVar, font: tuple = ('calibre', 10, 'normal')) -> (Entry, Variable):
         variable = variable_type()
         entry = Entry(self.window, textvariable=variable, font=font)
         entry.place(x=x_position, y=y_position, width=width, height=height)
         return entry, variable
 
-    def create_label(self, x_pos: int, y_pos: int, text: str = None, variable_type=None) -> (Entry, Optional[Variable]):
+    def create_label(self, x_pos: int, y_pos: int, text: str = None,
+                     variable_type=None, font: tuple = ("Arial", 8)) -> (Entry, Optional[Variable]):
         label: Label
         if variable_type:
-            variable = variable_type()
-            label = Label(self.window, textvariable=variable)
+            variable: Variable = variable_type()
+            variable.set("...") if variable_type == StringVar else variable.set(0)
+            label = Label(self.window, textvariable=variable, font=font)
             label.place(x=x_pos, y=y_pos)
             return label, variable
         elif text:
-            label = Label(self.window, text=text)
+            label = Label(self.window, text=text, font=font)
             label.place(x=x_pos, y=y_pos)
             return label
 
-    def create_text(self, height: int, width: int, x_pos: int, y_pos: int, with_state: bool = False) -> (Text, Optional[str]):
+    def create_text(self, x_pos: int, y_pos: int, height: int, width: int, with_state: bool = False) -> (Text, Optional[str]):
         if with_state:
             state = "normal"
             text = Text(self.window, height=height, width=width, state=state)
@@ -316,7 +238,6 @@ class Interface:
         self.buton_connect["state"] = NORMAL
 
     def start(self):
-        self.set_widgets()
         self.window.mainloop()
 
 
